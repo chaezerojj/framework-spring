16f0b71366bbc6f4f035ff24cc3c327904af1625
chaezerojj <yyzz1219@gmail.com>
2024-06-17 16:38:06 +0900

    240617

---
+++ TodoApp/src/main/java/com/todo/TodoCont.java
@@ -0,0 +1,53 @@
+package com.todo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import lombok.RequiredArgsConstructor;
+
+// 주소 받아서 서비스로 연결하는 역할 (코드 많이 생성하는 곳이 아님)
+
+@Controller
+@RequiredArgsConstructor
+public class TodoCont {
+
+	@Autowired
+	TodoService tService;
+
+	@GetMapping("/")
+//	@RequestMapping(value = "/", method = RequestMethod.GET) // 주소 Mapping하는 또 다른 방법
+	public String getTodos(Model model) {
+//		for (TodoEntity tEntity : tService.getTodos()) {
+//			System.out.println(tEntity);
+//		}
+//		List<TodoEntity> todos = tService.getTodos();
+		model.addAttribute("todos", tService.getTodos());
+		return "todos";
+	}
+	
+//	get: 주소창에 데이터 노출됨
+//	post: 주소창에 데이터 노출 안됨
+	
+	@PostMapping("/put")
+//	public String putTodo(@RequestParam TodoEntity tEntity) {
+	public String putTodo(TodoEntity tEntity) {
+		tEntity.setCompleted(false);
+		tService.putTodo(tEntity);
+		return "redirect:/";
+	}
+	
+	@GetMapping("/delete/{id}")
+	public String deleteTodo(@PathVariable Integer id) {
+		tService.deleteTodo(id);
+		return "redirect:/";
+	}
+}
---
+++ TodoApp/src/main/java/com/todo/TodoService.java
@@ -0,0 +1,32 @@
+package com.todo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor // 자동으로 롬복에서 생성자 대입되게 해줌
+public class TodoService {
+	
+	@Autowired
+	TodoRepo tRepo;
+	
+	// DB에 있는 전체 데이터 가져오기
+	public List<TodoEntity> getTodos() {
+		// 할일 가져와서 리턴
+		return tRepo.findAll();
+	}
+	
+	// 사용자에게 입력받은 데이터를 DB에 저장
+	public void putTodo(TodoEntity tEntity) {
+		tRepo.save(tEntity);
+	}
+	
+	// 받은 id값으로 삭제하기
+	public void deleteTodo(Integer id) {
+		tRepo.deleteById(id);
+	}
+}
