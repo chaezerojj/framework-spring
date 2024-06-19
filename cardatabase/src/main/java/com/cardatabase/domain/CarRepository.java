144baf4a74c8d8c7413512891c0afb89d6c44165
chaezerojj <yyzz1219@gmail.com>
2024-06-19 16:48:26 +0900

    240619

--- cardatabase/src/main/java/com/cardatabase/domain/CarRepository.java
+++ cardatabase/src/main/java/com/cardatabase/domain/CarRepository.java
@@ -3,9 +3,12 @@ package com.cardatabase.domain;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 public interface CarRepository extends JpaRepository<Car, Long> {
 //													<dto, 데이터타입>
 	// 브랜드로 자동차 검색
 	List<Car> findByBrand(String brand);
 }
+
